5fb002cbca8c117578663113ff3a55526f9af624
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 02:56:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//Hoppock, Zachary
+//personalInfo.cpp
+//9/12/2020
+//Input details about yourself
+//Version #1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string first_name;
+string last_name;
+int age;
+  cout << "Enter your first name" << endl;
+  cin >> first_name;
+
+  cout << "Enter your last name" << endl;
+  cin >> last_name;
+
+  cout << "Enter your age" << endl;
+  cin >> age;
+  return 0;
+}
